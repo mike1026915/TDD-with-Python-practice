d398330fa02ffa80f31605e4bad6be6eed3a88d9
mike fang <mike1026915.cs96@g2.nctu.edu.tw>
2017-04-04 18:48:55 +0800

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -17,6 +17,11 @@ class HomePageTest(TestCase):
         html = render_to_string("home.html")
         self.assertEqual(response.content.decode(), html)
 
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
@@ -24,11 +29,31 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
-        self.assertIn('A new list item', response.content.decode())
-        html = render_to_string("home.html", {'new_item_text': "A new list item"})
-        self.assertEqual(response.content.decode(), html)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
 
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -49,3 +74,4 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from lists.models import Item
 
 def home_page(request):
-    return render(request, "home.html", {'new_item_text': request.POST.get('item_text', '')})
+    if request.method == "POST":
+        new_item_text = request.POST["item_text"]
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+    else:
+        new_item_text = ''
+
+    items = Item.objects.all()
+    return render(request, "home.html", {'items': items})
