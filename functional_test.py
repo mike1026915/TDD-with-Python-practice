ccd50de8e08f2dd090a2def3370b365e441d410e
mike fang <mike1026915.cs96@g2.nctu.edu.tw>
2017-04-04 15:42:00 +0800

    Front page HTML now generated from a template

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -39,7 +40,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+             "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
