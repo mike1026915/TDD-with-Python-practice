6dba94688da62255e6302ab576ca177e5d6566f7
mike fang <mike1026915.cs96@g2.nctu.edu.tw>
2017-04-10 22:31:16 +0800

    Add model "List"

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     return render(request, "home.html")
@@ -12,5 +12,6 @@ def view_list(request):
 
 def new_list(request):
     new_item_text = request.POST["item_text"]
-    Item.objects.create(text=new_item_text)
+    list_ = List.objects.create()
+    Item.objects.create(text=new_item_text, list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
