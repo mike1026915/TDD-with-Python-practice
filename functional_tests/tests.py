7d13ce1c7b9152f8c39ed839de91ba79fb22db0b
mike fang <mike1026915.cs96@g2.nctu.edu.tw>
2017-05-01 17:54:23 +0800

    refactor templates to use a base template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -41,7 +41,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+') #1
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
