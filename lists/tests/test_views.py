94b0cdb6c6ef30178e83832999d087afe54e306e
mike fang <mike1026915.cs96@g2.nctu.edu.tw>
2017-06-13 20:01:19 +0800

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,9 +6,13 @@ from django.utils.html import escape
 
 from lists.views import home_page
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 class HomePageTest(TestCase):
 
+    maxDiff = None
+
+    """
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -16,9 +20,17 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        html = render_to_string("home.html")
-        self.assertEqual(response.content.decode(), html)
+        html = render_to_string("home.html", {'form': ItemForm()})
+        self.assertMultiLineEqual(response.content.decode(), html)
+    """
 
+    def test_home_page_renders_home_templates(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 class ListViewTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.core.exceptions import ValidationError
 
-
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 def home_page(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {'form': ItemForm()})
 
 
 def view_list(request, list_id):
