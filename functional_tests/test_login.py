f23df96481f76c28ada50426f5e6f21ce2f38a3f
mike fang <mike1026915.cs96@g2.nctu.edu.tw>
2017-07-22 22:05:38 +0800

    Prepare for the chapter 16

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -5,23 +5,16 @@ class LoginTest(FunctionalTest):
     def test_login_with_persona(self):
         # Edith goes to the awesome superlists site
         # and notices a "Sign in" link for the first time.
-        self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
-
-        # A Persona login box appears
-        self.switch_to_new_window('Mozilla Persona')  #1
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_login').click()
 
         # Edith logs in with her email address
-        ## Use mockmyid.com for test email
         self.browser.find_element_by_id(
             'authentication_email'  #2
         ).send_keys('edith@mockmyid.com') #3
-        self.browser.find_element_by_tag_name('button').click()
-
-        # The Persona window closes
-        self.switch_to_new_window('To-Do')
+        #self.browser.find_element_by_tag_name('button').click()
 
         # She can see that she is logged in
-        self.wait_for_element_with_id('logout')  #4
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
+        #self.wait_for_element_with_id('logout')  #4
+        #navbar = self.browser.find_element_by_css_selector('.navbar')
+        #self.assertIn('edith@mockmyid.com', navbar.text)
