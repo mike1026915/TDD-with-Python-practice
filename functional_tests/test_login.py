c276e6d60ebadd1e76e3dac657db36a61a70aa32
mike fang <mike1026915.cs96@g2.nctu.edu.tw>
2017-07-26 21:03:15 +0800

    placeholder test_my_lists and move login checkers into base

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.keys import Keys
+
 from .base import FunctionalTest
 
 class LoginTest(FunctionalTest):
@@ -5,16 +7,12 @@ class LoginTest(FunctionalTest):
     def test_login_with_persona(self):
         # Edith goes to the awesome superlists site
         # and notices a "Sign in" link for the first time.
-        self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_login').click()
-
         # Edith logs in with her email address
-        self.browser.find_element_by_id(
-            'authentication_email'  #2
-        ).send_keys('edith@mockmyid.com') #3
-        #self.browser.find_element_by_tag_name('button').click()
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_login').send_keys('edith@mockmyid.com')
+        self.browser.find_element_by_id('id_login').send_keys(Keys.ENTER)
+
 
         # She can see that she is logged in
-        #self.wait_for_element_with_id('logout')  #4
-        #navbar = self.browser.find_element_by_css_selector('.navbar')
-        #self.assertIn('edith@mockmyid.com', navbar.text)
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('edith@mockmyid.com', navbar.text)
