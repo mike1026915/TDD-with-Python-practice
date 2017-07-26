75cf817b9cc48504b59957ccada0772b604d6794
mike fang <mike1026915.cs96@g2.nctu.edu.tw>
2017-07-26 22:11:35 +0800

    Begin chapter 18

--- functional_tests/test_my_list.py
+++ functional_tests/test_my_list.py
@@ -26,7 +26,7 @@ class MyListsTest(FunctionalTest):
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         email = 'edith@example.com'
 
-        self.browser.get(self.server_url)
+        self.browser.get(self.live_server_url)
         self.wait_to_be_logged_out(email)
 
         # Edith is a logged-in user
--- superlists/settings.py
+++ superlists/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'lists',
     'accounts',
+    'functional_tests',
 ]
 
 MIDDLEWARE_CLASSES = [
