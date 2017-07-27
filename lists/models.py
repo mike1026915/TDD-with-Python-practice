cce25688f1049761b52629ba790790163118ea2a
mike fang <mike1026915.cs96@g2.nctu.edu.tw>
2017-07-28 00:18:32 +0800

    End of chapter 18

--- lists/models.py
+++ lists/models.py
@@ -5,6 +5,10 @@ from django.conf import settings
 class List(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
 
+    @property
+    def name(self):
+        return self.item_set.first().text
+
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
 
