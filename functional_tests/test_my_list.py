6bfbc43734ca0b80ea3870c884ff79083efb36b8
mike fang <mike1026915.cs96@g2.nctu.edu.tw>
2017-07-30 16:28:15 +0800

    Fix the driver to browser

--- functional_tests/test_my_list.py
+++ functional_tests/test_my_list.py
@@ -17,7 +17,7 @@ class MyListsTest(FunctionalTest):
         ## to set a cookie we need to first visit the domain.
         ## 404 pages load the quickest!
         self.browser.get(self.live_server_url + "/404_no_such_url/")
-        self.driver.delete_all_cookies()
+        self.browser.delete_all_cookies()
         self.browser.add_cookie(dict(
             name=settings.SESSION_COOKIE_NAME,
             value=session.session_key, #2
