0bc44840cc75718524b70451c55e209f9d35a4a1
mike fang <mike1026915.cs96@g2.nctu.edu.tw>
2017-07-30 16:21:16 +0800

    Add domain *.herokuapp.com

--- functional_tests/test_my_list.py
+++ functional_tests/test_my_list.py
@@ -21,6 +21,7 @@ class MyListsTest(FunctionalTest):
             name=settings.SESSION_COOKIE_NAME,
             value=session.session_key, #2
             path='/app',
+            domain='*.herokuapp.com'
         ))
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
