96cd1a578c4a3a17c8147684785edcca6162f0da
mike fang <mike1026915.cs96@g2.nctu.edu.tw>
2017-07-30 16:03:57 +0800

    Add domain

--- functional_tests/test_my_list.py
+++ functional_tests/test_my_list.py
@@ -20,7 +20,8 @@ class MyListsTest(FunctionalTest):
         self.browser.add_cookie(dict(
             name=settings.SESSION_COOKIE_NAME,
             value=session.session_key, #2
-            path=os.getcwd(),
+            path='/',
+            domain='tdd-superlist.herokuapp.com'
         ))
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
